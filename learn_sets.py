366892a8a0fd59a333eb632cfc8da819db450385
Olegioner <sitdh@mail.ru>
2024-09-18 19:53:25 +0700

    task_18.09_19:53

--- learn_sets.py
+++ learn_sets.py
@@ -1,3 +1,47 @@
+# files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py',
+#          'book.txT', 'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt',
+#          'split.pop', 'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git']
+#
+# set_words = {el.lower() for el in files if el[-4:].lower() == '.png'}
+# print(*sorted(set_words))
+
+
+
+
+# sentence = '''My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for
+# a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which,
+# if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know
+# those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed
+# by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'''
+# set_words = {el.strip(' .,:-?!();').lower() for el in sentence.split() if len(el.strip(' .,:-?!();')) < 4}
+# print(*sorted(set_words))
+
+
+
+
+
+# sentence = '''My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a
+# pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which,
+# if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended,
+# with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'''
+# set_words = {el.strip(' .,:-?!();').lower() for el in sentence.split()}
+# print(*sorted(set_words))
+
+
+
+
+# words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+# set_letter = {words[i][0].lower() for i in range(len(words))}
+# print(*sorted(set_letter))
+
+
+
+# items = [10, '30', 30, 10, '56', 34, '12', 90, 89, 34, 45, '67', 12, 10, 90, 23, '45', 56, '56', 1, 5, '6', 5]
+#
+# my_set = {int(el) for el in items}
+# print(*sorted(my_set))
+
+
 '''Даны по 10-балльной шкале оценки по биологии трех учеников. Напишите программу,
 которая выводит множество оценок, не встречающихся ни у одного из трех учеников.'''
 # my_set1 = (set(int(el) for el in input().split()))
