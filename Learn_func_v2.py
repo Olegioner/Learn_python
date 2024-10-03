06b4c59a833f9236c02ae9efd69d469651c446a8
Olegioner <sitdh@mail.ru>
2024-10-03 12:50:41 +0700

    task_0310_12:42

---
+++ Learn_func_v2.py
@@ -0,0 +1,8 @@
+'''Напишите функцию matrix(), которая создает, заполняет и возвращает матрицу заданного размера. '''
+# def matrix(n=1, m=None, value=0):
+#     if m is None:
+#         m = n
+#     return [[value for _ in range(n)] for _ in range(n)]
+
+
+
