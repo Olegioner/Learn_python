479445f3dc03831edc345f29593f99fdba6c65b9
Olegioner <sitdh@mail.ru>
2024-10-15 13:12:36 +0700

    task_1510_13:50

--- Learn_func_v2.py
+++ Learn_func_v2.py
@@ -1,18 +1,48 @@
-'''Task'''
+'''TASK'''
 from functools import reduce
 
-floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59, 34.23, 12.12, 4.67, 2.45, 9.32]
-words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
-numbers = [4, 6, 9, 23, 5]
+data = [['Tokyo', 35676000, 'primary'],
+        ['New York', 19354922, 'nan'],
+        ['Mexico City', 19028000, 'primary'],
+        ['Mumbai', 18978000, 'admin'],
+        ['Sao Paulo', 18845000, 'admin'],
+        ['Delhi', 15926000, 'admin'],
+        ['Shanghai', 14987000, 'admin'],
+        ['Kolkata', 14787000, 'admin'],
+        ['Los Angeles', 12815475, 'nan'],
+        ['Dhaka', 12797394, 'primary'],
+        ['Buenos Aires', 12795000, 'primary'],
+        ['Karachi', 12130000, 'admin'],
+        ['Cairo', 11893000, 'primary'],
+        ['Rio de Janeiro', 11748000, 'admin'],
+        ['Osaka', 11294000, 'admin'],
+        ['Beijing', 11106000, 'primary'],
+        ['Manila', 11100000, 'primary'],
+        ['Moscow', 10452000, 'primary'],
+        ['Istanbul', 10061000, 'admin'],
+        ['Paris', 9904000, 'primary']]
 
-# Исправьте этот код
-map_result = list(map(lambda num: round(num**2, 1), floats))
-filter_result = list(filter(lambda name: name if name == name[::-1] and len(name) > 4 else '', words))
-reduce_result = reduce(lambda num1, num2: num1 * num2, numbers, 1)
 
-print(map_result)
-print(filter_result)
-print(reduce_result)
+big_city = sorted(map(lambda el: el[0], filter(lambda x:x if x[2] == 'primary' and x[1] > 10**7 else '', data)))
+big_city_output = reduce(lambda x, y: x + y + ', ' if y != big_city[-1] else x + y, big_city , 'Cities: ')
+print(big_city_output)
+
+
+'''Task'''
+# from functools import reduce
+#
+# floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59, 34.23, 12.12, 4.67, 2.45, 9.32]
+# words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
+# numbers = [4, 6, 9, 23, 5]
+#
+# # Исправьте этот код
+# map_result = list(map(lambda num: round(num**2, 1), floats))
+# filter_result = list(filter(lambda name: name if name == name[::-1] and len(name) > 4 else '', words))
+# reduce_result = reduce(lambda num1, num2: num1 * num2, numbers, 1)
+#
+# print(map_result)
+# print(filter_result)
+# print(reduce_result)
 '''Напишите программу, которая с помощью функций filter() и map() отбирает из заданного списка numbers трёхзначные числа,
 дающие при делении на 5 остаток 2, и выводит их кубы, каждый в отдельной строке.'''
 #
