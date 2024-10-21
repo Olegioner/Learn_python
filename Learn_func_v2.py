f202a1aed63a3df089dda48197cb0276a65538f0
Olegioner <sitdh@mail.ru>
2024-10-21 16:24:45 +0700

    task_2110_16:20

--- Learn_func_v2.py
+++ Learn_func_v2.py
@@ -1,5 +1,5 @@
 '''Используя параллельную итерацию сразу по трем спискам countries, capitals и population выведите информацию о стране'''
-
+#
 # countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
 # capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
 # population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
