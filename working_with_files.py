9a696962e85c35fb5757159d9ac63e873bdfc5b8
Olegioner <sitdh@mail.ru>
2024-10-30 12:57:05 +0700

    task_3010_12:56

--- working_with_files.py
+++ working_with_files.py
@@ -1,3 +1,51 @@
+
+
+'''Вам доступен CSV-файл data.csv, содержащий информацию в csv формате. Напишите функцию read_csv для чтения данных из этого файла.
+Она должна возвращать список словарей, интерпретируя первую строку как имена ключей, а каждую последующую строку как значения этих ключей'''
+# def read_csv():
+#     with open('C:/new/data.csv', 'r', encoding='utf-8') as data:
+#         data_keys = [el.strip() for el in data.readline().split(',')]
+#         data_value = [el.strip().split(',') for el in data.readlines()]
+#     dict_list = []
+#     for i in range(len(data_value)):
+#         dict_list.append(dict(zip(data_keys, data_value[i])))
+#     return dict_list
+# print(read_csv())
+
+'''Напишите программу выводящую все страны, название которых начинается с буквы 'G',
+численность населения которых больше чем 500000 человек, не меняя их порядок.'''
+
+# with open('C:/new/population.txt', 'r', encoding='utf-8') as country:
+#     country_list = [[el.strip() for el in row.split('\t')] for row in country.read().split('\n')]
+# country_list_res = list(map(lambda x: list(map(lambda el: int(el) if el.isdigit() else el, x)), country_list))
+# for i in range(len(country_list_res)):
+#     if country_list_res[i][0].startswith('G') and country_list_res[i][1] > 500000:
+#         print(country_list_res[i][0])
+
+
+'''Вам доступны два текстовых файла first_names.txt и last_names.txt, один с именами, другой с фамилиями.
+Напишите программу, которая c помощью модуля random создает
+3 случайные пары имя + фамилия, а затем выводит их, каждую на отдельной строке.'''
+# from random import randrange
+# with open('C:/new/first_names.txt') as f_name, open('C:/new/last_names.txt') as l_name:
+#     first_names = [el.strip() for el in f_name.readlines()]
+#     last_names = [el.strip() for el in l_name.readlines()]
+# for i in range(3):
+#     print(first_names[randrange(len(first_names))], last_names[randrange(len(last_names))])
+
+'''Напишите программу, которая выводит количество букв латинского алфавита, слов и строк'''
+# with open('C:/new/file.txt', 'r', encoding='utf-8') as file:
+#     content_file = file.read()
+#     file.seek(0)
+#     content_list = file.readlines()
+# letter = [el.strip() for el in list(content_file)]
+# letter_clean = []
+# for el in letter:
+#     if el not in ' .,!?0123456789"':
+#         letter_clean.append(el)
+# words = [el for el in content_file.split()]
+# print('Input file contains:', f'{len(letter_clean)} letters', f'{len(words)} words', f'{len(content_list)} lines', sep='\n')
+
 '''Напишите программу, которая вычисляет сумму всех чисел, записанных в файле.'''
 
 # with open('C:/new/nums(1).txt', 'r', encoding='utf-8') as numbers:
