f9ab34ab1e62f12fea8a5671131ad9ebbaec0197
Olegioner <sitdh@mail.ru>
2024-11-01 17:14:56 +0700

    task_0111_17:14

--- povtor.py
+++ povtor.py
@@ -1,20 +1,17 @@
 '''Task2'''
-def same_parity(numbers):
-    num_list = []
-    if numbers == []:
-       return num_list
-    elif numbers[0] % 2 == 0:
-        num_list = list(filter(lambda x: x if x % 2 == 0 else '', numbers))
-    else:
-        num_list = list(filter(lambda x: x if x % 2 != 0 else '', numbers))
-    return num_list
-
-
-# print(same_parity([]))
-print(same_parity([6, 0, 67, -7, 10, -20]))
-# print(same_parity([-7, 0, 67, -9, 70, -29, 90]))
-
-
+# def same_parity(numbers):
+#     num_list = []
+#     if len(numbers) == 0:
+#        return num_list
+#     elif numbers[0] % 2 == 0:
+#         for el in numbers:
+#             if el % 2 == 0:
+#                 num_list.append(el)
+#     else:
+#         for el in numbers:
+#             if el % 2 != 0:
+#                 num_list.append(el)
+#     return num_list
 
 
 '''Task1'''
