78a07cc2ad4a226880fea421909decab715370d0
Olegioner <sitdh@mail.ru>
2024-11-07 14:24:52 +0700

    task_0711_14:24

--- Learn_dates.py
+++ Learn_dates.py
@@ -1,3 +1,134 @@
+'''Task 13'''
+
+
+'''Task 12'''
+# from datetime import datetime
+#
+# def is_available_date(booked_dates, date_for_booking):
+#     date_b = []
+#     if len(date_for_booking) == 10:
+#         date_b.append((datetime.strptime(date_for_booking, '%d.%m.%Y')).toordinal())
+#     else:
+#         d1, d2 = date_for_booking.split('-')
+#         d1 = datetime.strptime(d1, '%d.%m.%Y').toordinal()
+#         d2 = datetime.strptime(d2, '%d.%m.%Y').toordinal()
+#         for i in range(d1, d2+1):
+#             date_b.append(i)
+#     date_block = []
+#     for el in booked_dates:
+#         if len(el) == 10:
+#             date_block.append((datetime.strptime(el, '%d.%m.%Y')).toordinal())
+#         else:
+#             d1, d2 = el.split('-')
+#             d1 = datetime.strptime(d1, '%d.%m.%Y').toordinal()
+#             d2 = datetime.strptime(d2, '%d.%m.%Y').toordinal()
+#             for i in range(d1, d2+1):
+#                 date_block.append(i)
+#     return all(map(lambda x: 0 if x in date_block else 1, date_b))
+#
+#
+#
+# # TEST
+# assert is_available_date(['04.11.2021', '05.11.2021-09.11.2021'], '01.11.2021') == True
+# assert is_available_date(['04.11.2021', '05.11.2021-09.11.2021'], '01.11.2021-04.11.2021') == False
+# assert is_available_date(['04.11.2021', '05.11.2021-09.11.2021'], '06.11.2021') == False
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '12.11.2021') == False
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '09.11.2021') == False
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '15.11.2021') == False
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '17.11.2021') == False
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '22.11.2021-25.11.2021') == True
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '01.11.2021-04.11.2021') == False
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '02.11.2021-05.11.2021') == False
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '10.11.2021-14.11.2021') == False
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '03.11.2021-05.11.2021') == False
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '09.11.2021-10.11.2021') == False
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '06.11.2021-08.11.2021') == False
+# assert is_available_date(['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021'], '14.11.2021-22.11.2021') == False
+# assert is_available_date(['14.09.2022-14.10.2022'], '20.09.2022') == False
+# assert is_available_date(['14.09.2022-14.10.2022'], '14.11.2022') == True
+# assert is_available_date(['14.09.2022-14.10.2022'], '15.11.2022-16.11.2022') == True
+# assert is_available_date(['14.09.2022-14.10.2022'], '14.09.2022-22.09.2022') == False
+# assert is_available_date(['02.11.2021', '05.11.2021-09.11.2021'], '04.11.2021') == True
+# print('TESTS_OK')
+
+'''Task 11'''
+# from datetime import datetime
+# with open('C:/new/diary.txt', 'r', encoding='utf-8') as file:
+#     diary = [el.strip() for el in file.readlines()]
+#
+# dict_diary = {}
+# key = 0
+# for el in diary:
+#     if el == '':
+#         continue
+#     if el[0].isdigit():
+#         key = datetime.strptime(el, '%d.%m.%Y; %H:%M')
+#         dict_diary[key] = []
+#         continue
+#     dict_diary[key].append(el)
+#
+#
+#
+# for k,v in sorted(dict_diary.items()):
+#     print(k.strftime('%d.%m.%Y; %H:%M'))
+#     print(*v, sep='\n')
+#     print()
+
+'''Task 10'''
+# from datetime import datetime
+#
+# data = {'Дима': ('03.11.2021 09:31:18', '03.11.2021 11:41:28'),
+#         'Геор': ('01.11.2021 09:03:04', '01.11.2021 12:40:35'),
+#         'Анна': ('02.11.2021 04:41:54', '02.11.2021 05:39:40'),
+#         'Илина': ('02.11.2021 01:36:40', '02.11.2021 04:48:27'),
+#         'Герман': ('04.11.2021 07:51:19', '04.11.2021 09:53:53'),
+#         'Руслан': ('01.11.2021 11:26:06', '01.11.2021 12:56:24'),
+#         'Лера': ('03.11.2021 11:09:41', '03.11.2021 14:37:41'),
+#         'Егор': ('03.11.2021 05:29:38', '03.11.2021 06:01:59'),
+#         'Максим': ('05.11.2021 13:05:03', '05.11.2021 14:27:41'),
+#         'Саша': ('03.11.2021 04:14:26', '03.11.2021 05:10:58'),
+#         'Марина': ('05.11.2021 15:21:06', '05.11.2021 18:33:46')}
+# data_res = {}
+# for k in data.keys():
+#     data_res[k] = datetime.timestamp(datetime.strptime(data[k][1], '%d.%m.%Y %H:%M:%S')) - datetime.timestamp(datetime.strptime(data[k][0], '%d.%m.%Y %H:%M:%S'))
+#
+# key = [k for k,v in data_res.items() if v == min(data_res.values())]
+# print(*key)
+'''Task 9'''
+# from datetime import date, time, datetime
+#
+# dates = [date(1793, 8, 23), date(1410, 3, 11), date(804, 11, 12), date(632, 6, 4),
+#          date(295, 1, 23), date(327, 8, 24), date(167, 4, 16), date(229, 1, 24),
+#          date(1239, 2, 5), date(1957, 7, 14), date(197, 8, 24), date(479, 9, 6)]
+#
+# times = [time(7, 33, 27), time(21, 2, 10), time(17, 20, 47), time(20, 8, 59),
+#          time(12, 42, 56), time(15, 9, 57), time(17, 47, 9), time(9, 40, 2),
+#          time(11, 47, 1), time(17, 27, 10), time(17, 55, 40), time(9, 14, 9)]
+#
+# date_time = [datetime.combine(d, t) for d, t in zip(dates, times)]
+# print(*sorted(date_time, key=lambda x: x.second), sep='\n')
+'''Task 8'''
+# from datetime import datetime
+#
+# times_of_purchases = [datetime(2017, 10, 1, 12, 23, 25), datetime(2017, 10, 1, 15, 26, 26),
+#                       datetime(2017, 10, 1, 15, 42, 57), datetime(2017, 10, 1, 17, 49, 59),
+#                       datetime(2017, 10, 2, 6, 37, 10), datetime(2017, 10, 2, 6, 42, 53),
+#                       datetime(2017, 10, 2, 8, 56, 45), datetime(2017, 10, 2, 9, 18, 3),
+#                       datetime(2017, 10, 2, 12, 23, 48), datetime(2017, 10, 2, 12, 45, 5),
+#                       datetime(2017, 10, 2, 12, 48, 8), datetime(2017, 10, 2, 12, 10, 54),
+#                       datetime(2017, 10, 2, 19, 18, 10), datetime(2017, 10, 2, 12, 31, 45),
+#                       datetime(2017, 10, 3, 20, 57, 10), datetime(2017, 10, 4, 7, 4, 57),
+#                       datetime(2017, 10, 4, 7, 13, 31), datetime(2017, 10, 4, 7, 13, 42),
+#                       datetime(2017, 10, 4, 7, 21, 54), datetime(2017, 10, 4, 14, 22, 12),
+#                       datetime(2017, 10, 4, 14, 50), datetime(2017, 10, 4, 15, 7, 27),
+#                       datetime(2017, 10, 4, 12, 44, 49), datetime(2017, 10, 4, 12, 46, 41),
+#                       datetime(2017, 10, 4, 16, 32, 33), datetime(2017, 10, 4, 16, 34, 44),
+#                       datetime(2017, 10, 4, 16, 46, 59), datetime(2017, 10, 4, 12, 26, 6)]
+# count = len(times_of_purchases)
+# for el in times_of_purchases:
+#     if el.hour >= 12:
+#         count -= 1
+# print('До полудня') if len(times_of_purchases)/2 < count else print('После полудня')
 
 '''Task 7'''
 # from datetime import date
