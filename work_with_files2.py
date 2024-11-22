a512afb781f2838e75efc7a8ba29058d43e2737a
Olegioner <sitdh@mail.ru>
2024-11-22 12:42:12 +0700

    task_2211_12:42

---
+++ work_with_files2.py
@@ -0,0 +1,18 @@
+'''Task2'''
+# import sys
+# from datetime import datetime
+#
+# date_list = [datetime.strptime(el.strip(), '%Y-%m-%d') for el in sys.stdin]
+#
+# date_list = sorted(date_list)
+# count_day = (date_list[-1] - date_list[0]).days
+# print(count_day)
+
+
+
+
+'''Task1'''
+# import sys
+# a = [el.strip() for el in sys.stdin]
+# for el in a:
+#     print(el[::-1])
