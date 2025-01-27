d38b78370906fd578ccd450c25d256e56c0b90ba
Olegioner <sitdh@mail.ru>
2025-01-27 09:36:28 +0700

    task_2701_09:36

--- named_tuple.py
+++ named_tuple.py
@@ -1,3 +1,20 @@
+'''Task 6.4.12'''
+# from collections import namedtuple
+# from datetime import datetime
+# import csv
+#
+# with open('C:/new/meetings.csv',  'r', encoding='utf-8') as file:
+#     persons = list(csv.reader(file, delimiter=','))
+#     headers = persons.pop(0)
+#     Friends = namedtuple('Friends', headers)
+#     all_friends = []
+#     for el in persons:
+#         all_friends.append(Friends(*el))
+#
+# all_friends = sorted(all_friends, key=lambda x:datetime.strptime(f'{x.meeting_date} {x.meeting_time}', '%d.%m.%Y %H:%M'))
+# for el in all_friends:
+#     print(el.surname, el.name)
+
 '''Task 6.4.11'''
 # from collections import namedtuple
 #
@@ -18,9 +35,14 @@
 #          User('Tracy', 'Wallace', 'sblack@me.com', 'Silver'),
 #          User('Russell', 'Smith', 'isaacson@comcast.net', 'Bronze'),
 #          User('Megan', 'Patterson', 'hoangle@outlook.com', 'Basic')]
-
-a = ['Gold', 'Silver', 'Bronze', 'Basic' ]
-print(sorted(a))
+#
+# plans = ['Gold', 'Silver', 'Bronze', 'Basic' ]
+#
+# for el in sorted(sorted(users, key=lambda x: x.email), key=lambda x: plans.index(x.plan)):
+#     print(el.name, el.surname)
+#     print(f'  Email: {el.email}')
+#     print(f'  Plan: {el.plan}')
+#     print()
 
 
 '''Task 6.4.10'''
